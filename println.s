2e49fbbd3433dfdda7b8a7e95752c2c11808015a
OneAutumnMango <mangozoldyck@gmail.com>
2023-02-17 12:11:37 +0000

    Fixes issue causing lines to be printed backwards

--- println.s
+++ println.s
@@ -7,12 +7,14 @@ println:	# a0: line | a1: word
 	li s0, 0 		# current position (pos)
 	li s1, 0xffff8000    	# starting address of the graphics display
 	li s2, 0x80000000	# 0x80000000 = 0b1000...0
+	li s3, 0x00000001	
 	
 loop:
 	li t0, 32
 	bge s0, t0, exit  	# branch if pos >= 32
 	
-	srl t0, s2, s0  
+#	srl t0, s2, s0  	# mirrors along x-axis
+	sll t0, s3, s0 
 	and t0, t0, a1  	# checks bit at (pos) position
 
 	beqz t0, skip		# skip the write if t0 == 0	
@@ -29,6 +31,7 @@ write:
 	
 	lw t3, (t2)			# load word to write to
 	srl t4, s2, a0		# shift 0b100...0 >> line(a0) 
+#	sll t4, s3, a0		# shift 0b000..1 << line(a0)  -> mirrors along y-axis
 	or t3, t3, t4		# write bit from above to word
 	sw t3, (t2)			# save word back to addr
 	
@@ -40,4 +43,4 @@ write:
 	ret
 	
 exit:
-	jr s7
+	jr s7  			# return from program
