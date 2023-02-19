4c1550bb16182da3acf1b17564e73567c69d5ffb
OneAutumnMango <mangozoldyck@gmail.com>
2023-02-19 23:32:52 +0000

    Formatting

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define STARTPIX 0xffff8000
 
 extern void println(unsigned int line, unsigned int word);
-void next_line(unsigned int, int );
+void next_line(unsigned int, int);
 int start();
 
 int main() {
@@ -14,33 +14,31 @@ int main() {
 int start() {
     int y = 0;
     int word = 0;
-    for (;;)
-    {
-        unsigned int button = *(volatile unsigned int*)0xffff3100;
-        unsigned int joystick = *(volatile unsigned int*)0xffff3200;
-        unsigned int stop = *(volatile unsigned int*)0xffff3300;
-
+    for (;;) {
+        unsigned int button = *(volatile unsigned int *)0xffff3100;
+        unsigned int joystick = *(volatile unsigned int *)0xffff3200;
+        unsigned int stop = *(volatile unsigned int *)0xffff3300;
 
         for (int i = 0; i < 16; i++) {
             if (i == 0) {
-                *((volatile int *)(STARTPIX + y*0x4)) ^= 0x80000000; //on
-                *((volatile int *)(STARTPIX + y*0x4)) ^= 0x80000000; //off
+                *((volatile int *)(STARTPIX + y * 0x4)) ^= 0x80000000; // on
+                *((volatile int *)(STARTPIX + y * 0x4)) ^= 0x80000000; // off
             }
         }
 
         if (joystick) {
-            switch(joystick) {
+            switch (joystick) {
             case 0b1111:
-                if(y < WIDTH - 1) y+=1;
+                if (y < WIDTH - 1) y += 1;
                 break;
             case 0b0001:
-                if (y > 0) y-=1;
+                if (y > 0) y -= 1;
                 break;
             }
         }
 
         if (button) {
-            *((volatile int *)(STARTPIX + y*4)) ^= 0x80000000;
+            *((volatile int *)(STARTPIX + y * 4)) ^= 0x80000000;
             word ^= 0b1 << y;
         }
 
@@ -56,7 +54,6 @@ void next_line(unsigned int cur, int rule) { // works for any rule in range [0, 
 
     for (unsigned int j = 0; j < WIDTH; j++) { // each line per image
         cur = next;
-
 
         println(j, next);
         // printstr("\nline: "); // print line number to display
